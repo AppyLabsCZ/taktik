356bf196beebe44f382ec9ffe30e5e88dd8746c9
Michael Grznar <info@appylabs.cz>
2024-08-26 18:29:59 +0200

    Reduce API request throttling limit for testing

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\BookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:2,1'])->group(function () {
     Route::get('/books', [BookController::class, 'index']);
     Route::post('/new-book', [BookController::class, 'store']);
     Route::put('/update-book/{id}', [BookController::class, 'update']);
